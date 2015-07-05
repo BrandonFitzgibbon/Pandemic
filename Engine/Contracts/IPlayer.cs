3d031d59719baab583b4033c3893082ae11bd55d
Brandon Fitzgibbon <brandon_fitzgibbon@live.ca>
2015-07-05 18:21:00 -0400

    july 5th

---
+++ Engine/Contracts/IPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Contracts
+{
+    public interface IPlayer
+    {
+        string Name { get; set; }
+        ICity Location { get; set; }
+        IRole Role { get; }
+    }
+}
---
+++ Engine/Contracts/IRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Contracts
+{
+    public interface IRole
+    {
+        int Id { get; }
+        string Name { get; }
+    }
+}
